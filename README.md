7dd5e894830045dd0179396d5110e05488aefd29
sahil gehani <sahilgehani47@gmail.com>
2024-12-03 17:32:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+models used:
+mistral-7b-instruct-v0.2.Q5_K_M.gguf
---
+++ llm_chain.py
@@ -0,0 +1,11 @@
+from langchain.chains import StuffDocumentsChain,LLMChain,ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain.llms import ctransformers
+from langchain.vectorstores import Chroma
+import chromadb
+import yaml
+
+with open("config.yml","r") as f:
+    config = yaml.safe_load(f)
