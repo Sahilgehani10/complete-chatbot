8ead03659744f3745428879b182431d590c84836
sahil gehani <sahilgehani47@gmail.com>
2024-12-08 15:49:40 +0530

    chat implementation complete

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+from langchain_community.chat_message_histories import (
+    StreamlitChatMessageHistory,
+)
 import streamlit as st
+from llm_chain import load_normal_chain
+
+def load_chain(chat_history):
+    return load_normal_chain(chat_history)
 
 def clear_input_field():
     st.session_state.user_question=st.session_state.user_input
@@ -19,17 +26,21 @@ def main():
         st.session_state.send_input=False
         st.session_state.user_question=""
 
+    chat_history=StreamlitChatMessageHistory(key="history")
+    llm_chain=load_chain(chat_history)
     user_input=st.text_input("type your message here",key="user_input",on_change=set_send_input)
 
     send_button=st.button("Send",key="send_button")
 
     if send_button or st.session_state.send_input:
         if st.session_state.user_question!="":
-            llm_response="This is a response from the LLM model"
+            
 
             with chat_container:
                 st.chat_message("user").write(st.session_state.user_question)
-                st.chat_message("ai").write("here is a response from the LLM model")
+                llm_response=llm_chain.run(st.session_state.user_question)
+                st.chat_message("ai").write(llm_response)
+                st.session_state.user_question=""
 
 if __name__=="__main__":
     main()
